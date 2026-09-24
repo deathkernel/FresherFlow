ec746e1d7f14bac6e9979226fa7992cdeb2ac38a
Samarth Gadekar <112534680+deathkernel@users.noreply.github.com>
2026-09-24 08:45:02 +0530

    Use secure session cookies by default in production

--- config.py
+++ config.py
@@ -47,5 +47,8 @@ class Config:
 
     SESSION_COOKIE_HTTPONLY = True
     SESSION_COOKIE_SAMESITE = "Lax"
-    SESSION_COOKIE_SECURE = os.environ.get("SESSION_COOKIE_SECURE", "0") == "1"
+    SESSION_COOKIE_SECURE = (
+        os.environ.get("SESSION_COOKIE_SECURE", "1" if ENVIRONMENT == "production" else "0")
+        == "1"
+    )
     PERMANENT_SESSION_LIFETIME = timedelta(hours=8)
